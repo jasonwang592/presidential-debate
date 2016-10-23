d30e772bd120a58f22d957f651812b6100c41428
jason.wang <jason.wang@workday.com>
2016-10-22 22:26:25 -0700

    bar chart for word frequency of pos/neg words done!

--- debate.py
+++ debate.py
@@ -3,7 +3,8 @@ import pickle
 import matplotlib.pyplot as plt
 import string
 import codecs
-
+plt.style.use('fivethirtyeight')
+from nltk.corpus import stopwords
 
 def read_and_clean():
 	'''reads the debate csv and removes all Audience and non-candidate related text'''
@@ -19,8 +20,11 @@ def read_and_clean():
 	return clinton_df, trump_df
 
 def word_counter(dataFrame):
-	#return dictionaries of the candidates words and their respective counts
+	'''return dictionaries of the candidates words (stripped of stopwords
+	and their respective counts'''
 	word_list = list(dataFrame['Text'])
+	cachedStopWords = stopwords.words("english")
+
 
 	counter = {}
 	translator = str.maketrans({key: None for key in string.punctuation})
@@ -28,6 +32,7 @@ def word_counter(dataFrame):
 		word_list = line.translate(translator).split()
 		for word in word_list:
 			word = word.lower()
+			if word in cachedStopWords: continue
 			if word in counter.keys():
 				counter[word] = counter[word] + 1
 			else:
@@ -46,6 +51,7 @@ def sentiment_analysis(word_dict, total_words):
 	with open('positive-words.txt') as f:
 		for line in f:
 			word = str(line).rstrip()
+			if word == 'trump': continue
 			if word in word_dict.keys():
 				positive_counter += word_dict[word]
 				p_word_list[word] = word_dict[word]
@@ -61,6 +67,36 @@ def sentiment_analysis(word_dict, total_words):
 
 	return positive_rate, negative_rate, p_word_list, n_word_list
 
+def sentiment_chart(p_corpus, n_corpus, l_threshold = 0, h_threshold = 100):
+	'''Produces a horizontal bar chart displaying word and frequency data for a candidate
+		p_corpus: dictionary of positive words
+		n_corpus: dictionary of negative words
+		l_threshold: the minimum number of times a word must appear for it to be plotted
+		h_threshold: the maximum number of times a word can appear for it to be plotted
+	'''
+	p_corpus = {word:freq for word,freq in p_corpus.items() if freq > l_threshold and freq <= h_threshold}
+	n_corpus = {word:freq for word,freq in n_corpus.items() if freq > l_threshold and freq <= h_threshold}
+	n_corpus.update((x,y * -1) for x, y in n_corpus.items())
+	p_sorted = sorted(p_corpus.items(), key = lambda x: x[1])
+	n_sorted = sorted(n_corpus.items(), key = lambda x: x[1])
+
+	corpus =  list(zip(*n_sorted))[0] + list(zip(*p_sorted))[0]
+	freq = list(zip(*n_sorted))[1] + list(zip(*p_sorted))[1]
+
+	word_index = range(0, len(corpus))
+	df = pd.DataFrame()
+	df['value'] = freq
+
+	df['pos'] = [True if i > 0 else False for i in df['value']]
+	plt.figure(figsize=(15,10))
+	plt.barh(word_index, freq, color = df.pos.map({True: 'g', False:'r'}))
+	plt.ylim([0,len(corpus)])
+	plt.xlabel('Frequency')
+	plt.ylabel('Word')
+	plt.yticks(word_index, corpus)
+	plt.tight_layout()
+	plt.show()
+
 
 clinton_df = pickle.load(open('clinton.pickle', 'rb'))
 trump_df = pickle.load(open('trump.pickle', 'rb'))
@@ -71,37 +107,22 @@ trump_words = word_counter(trump_df)
 
 clinton_total_words = sum(clinton_words.values())
 trump_total_words = sum(trump_words.values())
-h_pos, h_neg, h_pos_words, h_neg_words = sentiment_analysis(clinton_words, clinton_total_words)
-t_pos, t_neg, t_pos_words, t_neg_words = sentiment_analysis(trump_words, trump_total_words)
+h_pos, h_neg, h_pos_words, h_neg_words = sentiment_analysis(clinton_words, sum(clinton_words.values()))
+t_pos, t_neg, t_pos_words, t_neg_words = sentiment_analysis(trump_words, sum(trump_words.values()))
 
-print("Out of %s words and %s Hillary and Trump spoke, respectively:" % (clinton_total_words, trump_total_words))
+print("Out of %s non-stopwords and %s Hillary and Trump spoke, respectively:" % (clinton_total_words, trump_total_words))
 print("Hillary's positive word rate: %0.2f" % h_pos + "%")
 print("Hillary's negative word rate: %0.2f" % h_neg + "%")
 print("Trump's positive word rate: %0.2f" % t_pos + "%")
 print("Trump's negative word rate: %0.2f" % t_neg + "%")
 print("Hillary spoke %s unique words while Trump spoke %s" % (len(clinton_words), len(trump_words)))
 
-print(sorted(t_neg_words.items(), key = lambda x: x[1], reverse = True))
-print(sorted(h_neg_words.items(), key = lambda x: x[1], reverse = True))
+sentiment_chart(h_pos_words, h_neg_words, 10, 50)
 
-clinton_tuples = sorted(clinton_words.items(), key = lambda x: x[1], reverse = True)
-trump_tuples = sorted(trump_words.items(), key = lambda x: x[1], reverse = True)
-
-# print(clinton_tuples[:50])
-# print(trump_tuples[:50])
-# words = list(zip(*clinton_tuples))[0]
-# freq = list(zip(*clinton_tuples))[1]
-# x_pos = np.arange(len(words))
-
-
-# plt.bar(x_pos,freq,align='center')
-# plt.xticks(x_pos, words)
-
-# plt.show()
-
-# print(sorted(trump_words.items(), key = lambda x: x[1]))
-
-
+# print(sorted(t_pos_words.items(), key = lambda x: x[1], reverse = True))
+# print(sorted(t_neg_words.items(), key = lambda x: x[1], reverse = True))
+# print(sorted(h_pos_words.items(), key = lambda x: x[1], reverse = True))
+# print(sorted(h_neg_words.items(), key = lambda x: x[1], reverse = True))
 
 
 
